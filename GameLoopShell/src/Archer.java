fb37abd498517c7655656af2f3b05f4633d1dc6b
701129001 <701129001@W829-1804.edu.cbe.ab.ca>
2018-05-29 14:40:15 -0600

    First push.

---
+++ GameLoopShell/src/Archer.java
@@ -0,0 +1,97 @@
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+public class Archer extends Sprite{
+	
+	@Override
+	public void update(KeyboardInput keyboard, long actual_delta_time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getMinX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMaxX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMinY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMaxY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMinX(double currentX) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMinY(double currentY) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBarriers(ArrayList<Rectangle> barriers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSprites(ArrayList<Sprite> staticSprites) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setKeyboard(KeyboardInput keyboard) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean checkCollisionWithSprites(double x, double y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkCollisionWithBarrier(double x, double y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
